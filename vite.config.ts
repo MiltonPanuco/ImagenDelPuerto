f642b37fe40b1c0391826ce25662ffb08f3d2378
juniorrivann <juniorrivann@hotmail.com>
2025-09-19 13:46:23 -0700

    core: Agrega más aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
@@ -27,6 +28,14 @@ export default defineConfig(({ mode }) => {
     }
 
     return {
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'resources/js'),
+                '@components': path.resolve(__dirname, 'resources/js/components'),
+                '@pages': path.resolve(__dirname, 'resources/js/pages'),
+                '@styles': path.resolve(__dirname, 'resources/css'),
+            },
+        },
         plugins: [
             laravel({
                 input: ['resources/css/app.css', 'resources/js/app.tsx'],
