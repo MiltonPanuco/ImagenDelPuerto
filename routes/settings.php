7df8d22ca4004ed3879b49d901a65d5edbddcc83
juniorrivann <juniorrivann@hotmail.com>
2025-10-09 15:56:30 -0700

    perf(login): Quita rutas innecesarias y cambia texto a español

--- routes/settings.php
+++ routes/settings.php
@@ -5,12 +5,12 @@ use App\Http\Controllers\Settings\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::middleware('auth')->group(function () {
-    Route::redirect('settings', '/settings/profile');
+Route::prefix('admin')->middleware('auth')->group(function () {
+    Route::redirect('settings', 'admin/settings/profile');
 
     Route::get('settings/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('settings/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('settings/password', [PasswordController::class, 'edit'])->name('password.edit');
 
